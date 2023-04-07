4fe1dc32a86493d845a80e736ed167c8fd8a4acc
colby Weber <cweber@wiktel.com>
2023-04-06 18:04:56 -0600

    trans ALMOST gets max points for 64x64

--- trans.c
+++ trans.c
@@ -30,71 +30,110 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-    int blocksWide;
-    int blocksHigh;
     // int temp;
-    int k0, k1, k2, k3, k4, k5, k6, k7;
+    int t0, t1, t2, t3, t4, t5, t6, t7;
+    int i, ii, j, jj;
 
+    // different methods worked better for different dimmentions
+
+    // if dimmentions not even
     if (M != N) {
-        blocksWide = M / BLOCK_SIZE_1;
-        blocksHigh = N / BLOCK_SIZE_1;
 
-        for (int i = 0; i < N; i+= BLOCK_SIZE_1) {
-            for (int j = 0; j < M; j+= BLOCK_SIZE_1) {
-                for (int jj = j; (jj < j + BLOCK_SIZE_1) && (jj < M); jj++) {
-                    for (int ii = i; (ii < i + BLOCK_SIZE_1) && (ii < N); ii++) {
+        // divide first by blocks
+        for (i = 0; i < N; i+= BLOCK_SIZE_1) {
+            for (j = 0; j < M; j+= BLOCK_SIZE_1) {
+                // sub elements in each block
+                for (jj = j; (jj < j + BLOCK_SIZE_1) && (jj < M); jj++) {
+                    for (ii = i; (ii < i + BLOCK_SIZE_1) && (ii < N); ii++) {
                         B[jj][ii] = A[ii][jj];
                     }
                 }
             }
         }
+    // if even dimmentions and <= 32
     } else if (M <= 32) {
-        blocksWide = M / BLOCK_SIZE_1;
-        blocksHigh = N / BLOCK_SIZE_1;
+        for (i = 0; i < (N / BLOCK_SIZE_1); i++) {
+            for (j = 0; j < (M / BLOCK_SIZE_1); j++) {
+                for (ii = i * BLOCK_SIZE_1; ii < i * BLOCK_SIZE_1 + BLOCK_SIZE_1; ii++) {
+                    t0 = A[ii][j * BLOCK_SIZE_1 + 0];
+                    t1 = A[ii][j * BLOCK_SIZE_1 + 1];
+                    t2 = A[ii][j * BLOCK_SIZE_1 + 2];
+                    t3 = A[ii][j * BLOCK_SIZE_1 + 3];
+                    t4 = A[ii][j * BLOCK_SIZE_1 + 4];
+                    t5 = A[ii][j * BLOCK_SIZE_1 + 5];
+                    t6 = A[ii][j * BLOCK_SIZE_1 + 6];
+                    t7 = A[ii][j * BLOCK_SIZE_1 + 7];
 
-        for (int i = 0; i < blocksHigh; i++) {
-            for (int j = 0; j < blocksWide; j++) {
-                for (int ii = i * BLOCK_SIZE_1; ii < i * BLOCK_SIZE_1 + BLOCK_SIZE_1; ii++) {
-                    k0 = A[ii][j * BLOCK_SIZE_1 + 0];
-                    k1 = A[ii][j * BLOCK_SIZE_1 + 1];
-                    k2 = A[ii][j * BLOCK_SIZE_1 + 2];
-                    k3 = A[ii][j * BLOCK_SIZE_1 + 3];
-                    k4 = A[ii][j * BLOCK_SIZE_1 + 4];
-                    k5 = A[ii][j * BLOCK_SIZE_1 + 5];
-                    k6 = A[ii][j * BLOCK_SIZE_1 + 6];
-                    k7 = A[ii][j * BLOCK_SIZE_1 + 7];
-
-                    B[j * BLOCK_SIZE_1 + 0][ii] = k0;
-                    B[j * BLOCK_SIZE_1 + 1][ii] = k1;
-                    B[j * BLOCK_SIZE_1 + 2][ii] = k2;
-                    B[j * BLOCK_SIZE_1 + 3][ii] = k3;
-                    B[j * BLOCK_SIZE_1 + 4][ii] = k4;
-                    B[j * BLOCK_SIZE_1 + 5][ii] = k5;
-                    B[j * BLOCK_SIZE_1 + 6][ii] = k6;
-                    B[j * BLOCK_SIZE_1 + 7][ii] = k7;
+                    B[j * BLOCK_SIZE_1 + 0][ii] = t0;
+                    B[j * BLOCK_SIZE_1 + 1][ii] = t1;
+                    B[j * BLOCK_SIZE_1 + 2][ii] = t2;
+                    B[j * BLOCK_SIZE_1 + 3][ii] = t3;
+                    B[j * BLOCK_SIZE_1 + 4][ii] = t4;
+                    B[j * BLOCK_SIZE_1 + 5][ii] = t5;
+                    B[j * BLOCK_SIZE_1 + 6][ii] = t6;
+                    B[j * BLOCK_SIZE_1 + 7][ii] = t7;
                 }
             }
         }
+    // if even and larger than 32
     } else {
-        blocksWide = M / BLOCK_SIZE_2;
-        blocksHigh = N / BLOCK_SIZE_2;
+        for (i = 0; i < (M / BLOCK_SIZE_1); i ++) {
+            for (j = 0; j < (N / BLOCK_SIZE_1); j ++) {
+                for (ii = 0; ii < 4; ii ++) {
+                    B[j * 8 + 0][i * 8 + 0 + ii] = A[i * 8 + ii][j * 8 + 0];
+                    B[j * 8 + 1][i * 8 + 0 + ii] = A[i * 8 + ii][j * 8 + 1];
+                    B[j * 8 + 2][i * 8 + 0 + ii] = A[i * 8 + ii][j * 8 + 2];
+                    B[j * 8 + 3][i * 8 + 0 + ii] = A[i * 8 + ii][j * 8 + 3];
+                    B[j * 8 + 0][i * 8 + 4 + ii] = A[i * 8 + ii][j * 8 + 4];
+                    B[j * 8 + 1][i * 8 + 4 + ii] = A[i * 8 + ii][j * 8 + 5];
+                    B[j * 8 + 2][i * 8 + 4 + ii] = A[i * 8 + ii][j * 8 + 6];
+                    B[j * 8 + 3][i * 8 + 4 + ii] = A[i * 8 + ii][j * 8 + 7];
+                }
 
-        for (int j = 0; j < blocksWide; j++) {
-            for (int i = 0; i < blocksHigh; i++) {
-                for (int ii = i * BLOCK_SIZE_2; ii < i * BLOCK_SIZE_2 + BLOCK_SIZE_2; ii++) {
-                    k0 = A[ii][j * BLOCK_SIZE_2 + 0];
-                    k1 = A[ii][j * BLOCK_SIZE_2 + 1];
-                    k2 = A[ii][j * BLOCK_SIZE_2 + 2];
-                    k3 = A[ii][j * BLOCK_SIZE_2 + 3];
+                for (ii = 0; ii < 4; ii ++) {
+                    t0 = B[j * 8 + ii][i * 8 + 4];
+                    t1 = B[j * 8 + ii][i * 8 + 5];
+                    t2 = B[j * 8 + ii][i * 8 + 6];
+                    t3 = B[j * 8 + ii][i * 8 + 7];
 
-                    B[j * BLOCK_SIZE_2 + 0][ii] = k0;
-                    B[j * BLOCK_SIZE_2 + 1][ii] = k1;
-                    B[j * BLOCK_SIZE_2 + 2][ii] = k2;
-                    B[j * BLOCK_SIZE_2 + 3][ii] = k3;
+                    B[j * 8 + ii][i * 8 + 4] = A[i * 8 + 4][j * 8 + ii];
+                    B[j * 8 + ii][i * 8 + 5] = A[i * 8 + 5][j * 8 + ii];
+                    B[j * 8 + ii][i * 8 + 6] = A[i * 8 + 6][j * 8 + ii];
+                    B[j * 8 + ii][i * 8 + 7] = A[i * 8 + 7][j * 8 + ii];
+
+                    B[j * 8 + 4 + ii][i * 8 + 0] = t0;
+                    B[j * 8 + 4 + ii][i * 8 + 1] = t1;
+                    B[j * 8 + 4 + ii][i * 8 + 2] = t2;
+                    B[j * 8 + 4 + ii][i * 8 + 3] = t3;
+
+                    B[j * 8 + 4 + ii][i * 8 + 4] = A[i * 8 + 4][j * 8 + 4 + ii];
+                    B[j * 8 + 4 + ii][i * 8 + 5] = A[i * 8 + 5][j * 8 + 4 + ii];
+                    B[j * 8 + 4 + ii][i * 8 + 6] = A[i * 8 + 6][j * 8 + 4 + ii];
+                    B[j * 8 + 4 + ii][i * 8 + 7] = A[i * 8 + 7][j * 8 + 4 + ii];
                 }
             }
         }
     }
+    // else {
+    //     blocksWide = M / BLOCK_SIZE_2;
+    //     blocksHigh = N / BLOCK_SIZE_2;
+
+    //     for (j = 0; j < blocksWide; j++) {
+    //         for (i = 0; i < blocksHigh; i++) {
+    //             for (ii = i * BLOCK_SIZE_2; ii < i * BLOCK_SIZE_2 + BLOCK_SIZE_2; ii++) {
+    //                 t0 = A[ii][j * BLOCK_SIZE_2 + 0];
+    //                 t1 = A[ii][j * BLOCK_SIZE_2 + 1];
+    //                 t2 = A[ii][j * BLOCK_SIZE_2 + 2];
+    //                 t3 = A[ii][j * BLOCK_SIZE_2 + 3];
+
+    //                 B[j * BLOCK_SIZE_2 + 0][ii] = t0;
+    //                 B[j * BLOCK_SIZE_2 + 1][ii] = t1;
+    //                 B[j * BLOCK_SIZE_2 + 2][ii] = t2;
+    //                 B[j * BLOCK_SIZE_2 + 3][ii] = t3;
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 /* 
