7a1469a97da29942a6676f65c78f3ec8e40c6104
Hayden Beames <hbeames7@gmail.com>
2023-04-06 19:11:04 -0600

    ecstacy

--- csim.c
+++ csim.c
@@ -31,8 +31,11 @@ uint64_t lru_count = 1;
 int hit_count = 0;
 int miss_count = 0;
 int eviction_count = 0;
+unsigned long long instr_count = 0;
 
 //prints all argument options when called
+
+//print options function
 void printOptions(char* argv[]) {
     printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", argv[0]);
     printf("Options:\n");
@@ -48,12 +51,14 @@ void printOptions(char* argv[]) {
     exit(0);
 }
 
+//cache block struct
 typedef struct {
     int valid;
     uint64_t lru;
     uint64_t tag;
 } block_t;
 
+//funciton to initalize the cache
 void initCache(block_t cache[S][E]) {
     for (int i = 0; i < S; i++) {
         for (int j = 0; j < E; j++) {
@@ -64,73 +69,50 @@ void initCache(block_t cache[S][E]) {
     }
 }
 
-
-void cacheAccess(block_t cache[S][E], unsigned long long address, int s, int b, char trace_line, unsigned long long instr_count) {
+//function simulating a cache access for multiple types of accesses
+void cacheAccess(block_t cache[S][E], unsigned long long address, int s, int b, char trace_line) {
     unsigned long long set = (address << ((sizeof(address)*8 - s - b))) >> ((sizeof(address)*8 - s));
     uint64_t tag = address >> (s + b);
     unsigned long long block_2_evict = 0;
     uint64_t evict_lru_maximum = 0;
 
-    for (int i = 0; i < E; i++) {
-        //printf("%jd ", cache[set][i].lru);
-        //printf("          ");
-    }
-    printf("address: %llx     set: %llx    s: %d     b: %d      ", address, set, s,b);
-    /*
+    // if M type access, increase hit count and update lru counters
     if (trace_line == 'M') {
         hit_count++;
+        //update lru counters
         for (int j = 0; j < E; j++) {
-               cache[set][j].lru++;
+            cache[set][j].lru++;
         }
     }
-    */
 
     //check for hit, if hit set LRU counter to instr_count and return
     for (int i = 0; i < E; i++) {
-        if (cache[set][i].valid && (cache[set][i].tag == tag)) {
+        //check if valid and tag match
+        if ((cache[set][i].valid == 0) || (cache[set][i].tag != tag)) continue;
+            //update lru counters
+            for (int j = 0; j < E; j++) {
+                cache[set][j].lru++;
+            } 
             cache[set][i].lru = instr_count;
             cache[set][i].tag = tag;
             hit_count++;
-            for (int i = 0; i < E; i++) {
-                printf("%ju ", cache[set][i].lru);
-                printf("      ");
-            }
-            for (int i = 0; i < E; i++) {
-                printf("%x ", cache[set][i].valid);
-                printf(" ");
-            }
-            printf("HIT\n");
-            for (int j = 0; j < E; j++) {
-                cache[set][j].lru++;
-            }
+
             return;
-        }
     }
-    
 
     evict_lru_maximum = cache[set][0].lru;
-    for (int i = 0; i < E; i++) {
-        // only update some of the lru counters (ones that are above)      
+    
+    //this loop iterates over all lru counters
+    for (int i = 0; i < E; i++) {  
+        //grab max lru value to decide which block to evict
         if (cache[set][i].lru < evict_lru_maximum) {
             evict_lru_maximum = cache[set][i].lru;
             block_2_evict = i;
         }
     }
 
-    for (int i = 0; i < E; i++) {
-        printf("%ju ", cache[set][i].lru);
-        printf("      ");
-    }
-
-    for (int i = 0; i < E; i++) {
-        printf("%x ", cache[set][i].valid);
-        printf(" ");
-    }
-    
-    
     //find block to evict in set
     if (cache[set][block_2_evict].valid) {
-        printf("BLOCK_2_EVICT: %llx\n", block_2_evict);
         eviction_count++;
     }
 
@@ -141,23 +123,20 @@ void cacheAccess(block_t cache[S][E], unsigned long long address, int s, int b, 
 
     miss_count++;
 
+    //update cache after loading in missed access
     cache[set][block_2_evict].lru = instr_count;
     cache[set][block_2_evict].tag = tag;
     cache[set][block_2_evict].valid = 1;
-    printf("\n");
 }
 
-void traceFile(char* traceFile) {
-
-    
-}
-
+//main function call
 int main(int argc, char *argv[])
 {
     //ARGUMENT CHECKS
     char opt;
     //all arg check
     while((opt = getopt(argc, argv, "s:E:b:t:vh")) != -1) { 
+        //switch statement for input operand
         switch(opt) 
         { 
             case 's': 
@@ -192,46 +171,47 @@ int main(int argc, char *argv[])
 
     //initialize cache
     initCache(cache);
+
+    //open file
     FILE* fp;
     fp = fopen(tracefile, "r");
+
     unsigned long long address;
-    unsigned long long instr_count = 0;;
     char trace_line;
     int size;
 
+    //scanf the file type of cache access
     while (fscanf(fp, " %c %llx,%d", &trace_line, &address, &size) == 3) {
-
-        
+        //switch statement for access type
         switch(trace_line) {
             case 'I':
                 //do nothing    
                 break;
             case 'L': 
                 instr_count++;
-                cacheAccess(cache, address, s, b, trace_line, instr_count);
+                cacheAccess(cache, address, s, b, trace_line);
                 //printf("L");
                 break;
             case 'S':
                 instr_count++;
-                cacheAccess(cache, address, s, b, trace_line, instr_count);
+                cacheAccess(cache, address, s, b, trace_line);
                 //printf("S");
                 break;
             case 'M':
                 instr_count++;
-                cacheAccess(cache, address, s, b, trace_line, instr_count);
-                instr_count++;
-                cacheAccess(cache, address, s, b, trace_line, instr_count);
+                cacheAccess(cache, address, s, b, trace_line);
+                
                 //printf("M");
                 break;
             default:
                 break;
         }
     }
+    //close file
     fclose(fp);
-    
 
-    
-    traceFile(tracefile);
+    //print cache access statistics
     printSummary(hit_count, miss_count, eviction_count);
+    
     return 0;
 }
